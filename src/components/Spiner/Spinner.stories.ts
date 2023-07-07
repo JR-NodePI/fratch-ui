f4d73446b9e51d9ff5335b0fe12789f2ec778634
JorgeRojo <jrojo@redpoints.com>
2023-07-07 06:29:20 +0200

    Add full builded modules isolation

---
+++ src/components/Spiner/Spinner.stories.ts
@@ -0,0 +1,16 @@
+import Spinner from "./Spinner";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Example/Spinner",
+  component: Spinner,
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof Spinner>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
