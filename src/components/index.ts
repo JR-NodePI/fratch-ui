4f9734c2819c290cca5502507b0b2c8eb933d609
JorgeRojo <jrojo@redpoints.com>
2023-07-13 13:40:53 +0200

    v1.0.47

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button/Button';
 export { default as Select } from './Form/Select/Select';
 export * as SelectProps from './Form/Select/SelectProps';
 export { default as Spinner } from './Spinner/Spinner';
+export { default as Header } from './Header/Header';
