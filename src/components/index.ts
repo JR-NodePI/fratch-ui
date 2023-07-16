a3488da923aa81cf6fc4f74275182d2b4ab5e888
JorgeRojo <jrojo@redpoints.com>
2023-07-16 23:39:42 +0200

    v1.0.72

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export { default as Button } from './Button/Button';
-export { default as Select } from './Form/Select/Select';
-export { default as LeftLabeledField } from './Form/LeftLabeledField/LeftLabeledField';
-export * as SelectProps from './Form/Select/SelectProps';
-export { default as Spinner } from './Spinner/Spinner';
 export { default as Header } from './Header/Header';
+export { default as InputCheck } from './Form/InputCheck/InputCheck';
+export { default as InputText } from './Form/InputText/InputText';
+export { default as LeftLabeledField } from './Form/LeftLabeledField/LeftLabeledField';
+export { default as Select } from './Form/Select/Select';
+export { default as Spinner } from './Spinner/Spinner';
+export * as SelectProps from './Form/Select/SelectProps';
