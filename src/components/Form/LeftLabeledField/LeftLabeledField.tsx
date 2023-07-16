f55bf16dc1c19329194b77aeff06b1baab2b920e
JorgeRojo <jrojo@redpoints.com>
2023-07-16 22:26:47 +0200

    v1.0.67

--- src/components/Form/LeftLabeledField/LeftLabeledField.tsx
+++ src/components/Form/LeftLabeledField/LeftLabeledField.tsx
@@ -4,15 +4,15 @@ import styles from './LeftLabeledField.module.css';
 
 function LeftLabeledField({
   label,
-  filed,
+  field,
 }: {
   label: JSX.Element;
-  filed: JSX.Element;
+  field: JSX.Element;
 }): JSX.Element {
   return (
     <div className={c(styles.wrapper)}>
       <div className={c(styles.label_container)}>{label}</div>
-      <div className={c(styles.field_container)}>{filed}</div>
+      <div className={c(styles.field_container)}>{field}</div>
     </div>
   );
 }
