b42c8c23763c7279982af10ffdf8db3d3a0a9b83
JorgeRojo <jrojo@redpoints.com>
2023-07-20 02:55:04 +0200

    v1.0.92

--- src/components/Modal/ModalProps.ts
+++ src/components/Modal/ModalProps.ts
@@ -12,8 +12,7 @@ export const ModalCloseTypes = {
   CLOSE: 'close' as const,
 };
 
-export type ModalCloseType =
-  (typeof ModalCloseTypes)[keyof typeof ModalCloseTypes];
+export type ModalCloseType = (typeof ModalCloseTypes)[keyof typeof ModalCloseTypes];
 
 export type ModalProps = {
   acceptButtonLabel?: string;
