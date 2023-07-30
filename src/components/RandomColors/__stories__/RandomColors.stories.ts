a8365fd3b1f75b3a9c1b6685e5ff8ae26d7b9759
JorgeRojo <jrojo@redpoints.com>
2023-07-30 19:58:40 +0200

    Add random colors

---
+++ src/components/RandomColors/__stories__/RandomColors.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import StoryRenderer from './StoryRenderer';
+
+const meta = {
+  title: 'Example/RandomColors',
+  component: StoryRenderer,
+  argTypes: {},
+} satisfies Meta<typeof StoryRenderer>;
+
+export default meta;
+
+type Story = StoryObj<typeof StoryRenderer>;
+
+export const RandomColors: Story = {
+  args: {},
+};
