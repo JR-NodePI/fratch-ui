db37d575d6f5da52ee95f732eb727662ff8e784c
Jorge Rojo <jrojo@redpoints.com>
2023-08-17 10:03:22 +0200

    Add tests for usePersistedColorScheme

--- src/components/ColorScheme/__tests__/usePersistedColorScheme.test.tsx
+++ src/components/ColorScheme/__tests__/usePersistedColorScheme.test.tsx
@@ -6,20 +6,20 @@ import { describe, it } from 'vitest';
 import usePersistedColorScheme from '../usePersistedColorScheme';
 
 describe('usePersistedColorScheme', () => {
-  const defaultColorScheme = 'light';
-
   afterEach(() => {
     globalThis.localStorage.clear();
   });
 
   it('should return the defaultColorScheme and colorScheme setter', () => {
+    const defaultColorScheme = 'light';
     const { result } = renderHook(() =>
       usePersistedColorScheme(defaultColorScheme)
     );
     expect(result.current).toEqual([defaultColorScheme, expect.any(Function)]);
   });
 
-  it('should return the colorScheme from localStorage', () => {
+  it('should return the dark colorScheme from localStorage', () => {
+    const defaultColorScheme = 'light';
     const storedColorScheme = 'dark';
     globalThis.localStorage.setItem('fratch-color-scheme', storedColorScheme);
 
@@ -31,7 +31,21 @@ describe('usePersistedColorScheme', () => {
     expect(result.current).toEqual([storedColorScheme, expect.any(Function)]);
   });
 
-  it('should set colorScheme to localStorage', () => {
+  it('should return the light colorScheme from localStorage', () => {
+    const defaultColorScheme = 'dark';
+    const storedColorScheme = 'light';
+    globalThis.localStorage.setItem('fratch-color-scheme', storedColorScheme);
+
+    const { result } = renderHook(() =>
+      usePersistedColorScheme(defaultColorScheme)
+    );
+
+    expect(defaultColorScheme).not.toEqual(storedColorScheme);
+    expect(result.current).toEqual([storedColorScheme, expect.any(Function)]);
+  });
+
+  it('should set dark colorScheme to localStorage', () => {
+    const defaultColorScheme = 'light';
     const colorSchemeToStore = 'dark';
 
     const { result } = renderHook(() =>
