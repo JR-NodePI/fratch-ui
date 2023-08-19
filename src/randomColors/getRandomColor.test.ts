15bffdea2ea43901e68c3d61095c1b124b1a2dd5
JorgeRojo <jrojo@redpoints.com>
2023-08-20 00:19:21 +0200

    Add getRandomColor tests

---
+++ src/randomColors/getRandomColor.test.ts
@@ -0,0 +1,21 @@
+import { CSSProperties } from 'react';
+
+import { describe, expect, it } from 'vitest';
+
+import { AVAILABLE_COLOR_LIST } from './constants';
+import getRandomColor from './getRandomColor';
+
+describe('getRandomColor', () => {
+  it('should go over the list of available color sorting randomly', () => {
+    const usedColors: CSSProperties['color'][] = [];
+
+    AVAILABLE_COLOR_LIST.forEach(() => {
+      const randomColor = getRandomColor(usedColors);
+
+      expect(AVAILABLE_COLOR_LIST).toContain(randomColor);
+      expect(usedColors).not.toContain(randomColor);
+
+      usedColors.push(randomColor);
+    });
+  });
+});
