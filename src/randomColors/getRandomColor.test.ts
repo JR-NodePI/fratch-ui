e7b4db1c15cf5ebf85edb633b2256264d8492464
JorgeRojo <jrojo@redpoints.com>
2023-08-20 01:22:30 +0200

    Improve getRandomColor tests

--- src/randomColors/getRandomColor.test.ts
+++ src/randomColors/getRandomColor.test.ts
@@ -7,15 +7,15 @@ import getRandomColor from './getRandomColor';
 
 describe('getRandomColor', () => {
   it('should go over the list of available color sorting randomly', () => {
-    const usedColors: CSSProperties['color'][] = [];
+    let usedColors: CSSProperties['color'][];
 
     AVAILABLE_COLOR_LIST.forEach(() => {
       const randomColor = getRandomColor(usedColors);
 
       expect(AVAILABLE_COLOR_LIST).toContain(randomColor);
-      expect(usedColors).not.toContain(randomColor);
+      expect(usedColors ?? []).not.toContain(randomColor);
 
-      usedColors.push(randomColor);
+      usedColors = [...(usedColors ?? []), randomColor];
     });
   });
 });
