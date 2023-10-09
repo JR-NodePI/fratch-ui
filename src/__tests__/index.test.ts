524e8cffae3b1e549268239d0d8f254618f06702
JorgeRojo <jorge.rojodiseno@gmail.com>
2023-10-09 20:07:18 +0200

    Fix header icon

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,9 +1,13 @@
 import { describe, expect, it } from 'vitest';
 
-import * as components from '../index';
+import * as components from '../components';
+import * as helpers from '../helpers';
 
 describe('index', () => {
   it('should have a list of components', () => {
     expect(components).toMatchSnapshot();
+  });
+  it('should have a list of helpers', () => {
+    expect(helpers).toMatchSnapshot();
   });
 });
